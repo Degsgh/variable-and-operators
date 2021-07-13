660b66d294ca4813907794b9203c8c528123d495
degsgh <bobbywekesa@gmail.com>
2021-07-13 16:27:10 +0300

    implement operator

---
+++ Operator.py
@@ -0,0 +1,19 @@
+'''
+calculate age of user based on current year and year of birth
+'''
+currentYear=2021
+yearofbirth=2000
+age=0
+
+age=currentYear -yearofbirth
+
+print(age)
+
+'''
+Given the exclusive price of milk and the VAT tax calculate the inclusive price
+'''
+exclPrice = 45.34
+vatTax = 16/100
+vatAmount=exclPrice * vatTax
+
+print(inclPrice)
---
+++ scope.py
@@ -0,0 +1,6 @@
+'''
+Wafula is the manager of Tuskys pioneer.Being a manager Wafula approaches you as a software developer to develop small python application to calculate the products in his supermarket.
+'''
+manager="Wafula"
+productName="Milk"
+InclPrice=50.00
